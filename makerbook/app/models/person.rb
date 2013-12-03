f7ee6442949bfc26e1ccf0f9ebde5b77702b3626
LesleyMdCampo <lesliz32@gmail.com>
2013-12-03 16:37:06 -0600

    Add full_name method. Add description field to database and populated it using a loop.

--- makerbook/app/models/person.rb
+++ makerbook/app/models/person.rb
@@ -1,5 +1,8 @@
 class Person < ActiveRecord::Base
 
-	
+	def full_name
+		self.first_name + " " + self.last_name
+	end
+
 
 end
--- makerbook/db/schema.rb
+++ makerbook/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203210022) do
+ActiveRecord::Schema.define(version: 20131203222003) do
 
   create_table "people", force: true do |t|
     t.string   "first_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20131203210022) do
     t.string   "picture_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
 end
