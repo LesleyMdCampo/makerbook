90453a3379c38d45872d9e4af21bb5c45e04e993
LesleyMdCampo <lesliz32@gmail.com>
2013-12-03 15:11:51 -0600

    Add rails app. Add Hirb to gemfile. Add 6 people to Person model.

---
+++ makerbook/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Makerbook::Application.config.secret_key_base = '8aa095d6a41ffc3d9e39176942150a16ecf37d5505a769a4f439a37fea1d49960c222ed6b1a9593219687058b90b2b5587d1ed39eadea4cada9f92395a086e7a'
